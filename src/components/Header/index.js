5a635ec837ceae54f339e77dd0bf97c4447eaa4c
vivian <vivian.v18723@gmail.com>
2022-11-07 21:10:31 +0000

    add nav bar with links, icons &... ...logo image in Header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,19 @@
+import earthImg from "../../assets/images/earth.png";
+
 function Header() {
   return (
     <header>
-      <p>This is a header</p>
+      <nav>
+        <img src={earthImg} alt="company logo" />
+        <a href="#home">Home</a>
+        <a href="#aboutUs">About Us</a>
+        <a href="#ourSuppliers">Our Suppliers</a>
+        <a href="#ourValues">Our Values</a>
+        <a href="#products">Products</a>
+        <a href="#contactUs">Contact Us</a>
+        <i className="fa-solid fa-cart-shopping"></i>
+        <i className="fa-solid fa-user"></i>
+      </nav>
     </header>
   );
 }
