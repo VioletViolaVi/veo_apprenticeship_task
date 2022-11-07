640226f1d9bd33959b63b73563aa083ea809fd8a
vivian <vivian.v18723@gmail.com>
2022-11-07 19:18:52 +0000

    import Footer, Header & ProductBlocks components create & export Home function add imported components to Home function

---
+++ src/pages/Home/index.js
@@ -0,0 +1,13 @@
+import { Footer, Header, ProductBlocks } from "../../components";
+
+function Home() {
+  return (
+    <>
+      <Header />
+      <ProductBlocks />
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
