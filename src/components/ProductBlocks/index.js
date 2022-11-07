fe4b5c2b5e4cd23ab05221e1c92e051f9684fa1c
vivian <vivian.v18723@gmail.com>
2022-11-07 19:13:28 +0000

    create & export function for product blocks

---
+++ src/components/ProductBlocks/index.js
@@ -0,0 +1,14 @@
+function ProductBlocks() {
+  return (
+    <header>
+      <p>This is for the product blocks</p>
+      <ul>
+        <li>product 1</li>
+        <li>product 2</li>
+        <li>product 3</li>
+      </ul>
+    </header>
+  );
+}
+
+export default ProductBlocks;
