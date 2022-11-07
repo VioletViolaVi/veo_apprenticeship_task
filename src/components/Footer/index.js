c6a7effb1e6f2875cb15e32d8fa3e23051fba030
vivian <vivian.v18723@gmail.com>
2022-11-07 21:18:18 +0000

    add content for Footer component

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,50 @@
 function Footer() {
   return (
     <footer>
-      <p>This is a footer</p>
+      <section>
+        <div>
+          <ul>
+            <li>
+              <h4>Partnership</h4>
+            </li>
+            <li>Websites</li>
+            <li>Social Media</li>
+            <li>Branding</li>
+          </ul>
+        </div>
+
+        <div>
+          <ul>
+            <li>
+              <h4>About</h4>
+            </li>
+            <li>Our Projects</li>
+            <li>Careers</li>
+          </ul>
+        </div>
+
+        <div>
+          <ul>
+            <li>
+              <h4>Support</h4>
+            </li>
+            <li>Support Request</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+      </section>
+
+      <hr />
+
+      <section>
+        <div>
+          <p>All rights reserved 2022</p>
+          <i class="fa-brands fa-twitter"></i>
+          <i class="fa-brands fa-facebook-f"></i>
+          <i class="fa-brands fa-instagram"></i>
+          <i class="fa-brands fa-youtube"></i>
+        </div>
+      </section>
     </footer>
   );
 }
