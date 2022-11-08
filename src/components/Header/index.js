a955f394d216037fd785f088fac16840da810284
vivian <vivian.v18723@gmail.com>
2022-11-08 16:32:10 +0000

    import css file add class names to elements use react hook to toggle nav links appearance

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,19 +1,40 @@
+import React, { useState } from "react";
+
 import earthImg from "../../assets/images/earth.png";
+import "./Header.css";
 
 function Header() {
+  const [showLinks, setShowLinks] = useState(false);
+
+  const handleClick = () => {
+    setShowLinks((prevShowLinks) => {
+      return !prevShowLinks;
+    });
+  };
+  console.log(showLinks);
+
   return (
-    <header>
-      <nav>
-        <img src={earthImg} alt="company logo" />
-        <a href="#home">Home</a>
-        <a href="#aboutUs">About Us</a>
-        <a href="#ourSuppliers">Our Suppliers</a>
-        <a href="#ourValues">Our Values</a>
-        <a href="#products">Products</a>
-        <a href="#contactUs">Contact Us</a>
-        <i className="fa-solid fa-cart-shopping"></i>
-        <i className="fa-solid fa-user"></i>
-      </nav>
+    <header className="header-bg">
+      <i className="fa-solid fa-bars" onClick={handleClick}></i>
+
+      {showLinks ? (
+        <nav className="nav-container">
+          <a href="#home">Home</a>
+          <a href="#aboutUs">About Us</a>
+          <a href="#ourSuppliers">Our Suppliers</a>
+          <a href="#ourValues">Our Values</a>
+          <a href="#products">Products</a>
+          <a href="#contactUs">Contact Us</a>
+        </nav>
+      ) : null}
+
+      <img src={earthImg} alt="company logo" className="logo-img" />
+
+      <i className="fa-solid fa-user"></i>
+
+      {/* <div>
+        <i className="fa-solid fa-cart-shopping"></i>      
+      </div> */}
     </header>
   );
 }
