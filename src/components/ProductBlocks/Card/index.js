7c4a45f216f752a60b4c2e28039a68360f00d5b0
vivian <vivian.v18723@gmail.com>
2022-11-08 19:23:08 +0000

    create overlapping background & add class name

--- src/components/ProductBlocks/Card/index.js
+++ src/components/ProductBlocks/Card/index.js
@@ -2,11 +2,16 @@ import "./Card.css";
 
 function Card({ title, image, description }) {
   return (
-    <section className="card-container">
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </section>
+    <>
+      <section className="card-container">
+        <img src={image} alt={title} />
+
+        <div className="card-placement-mat">
+          <h3>{title}</h3>
+          <p>{description}</p>
+        </div>
+      </section>
+    </>
   );
 }
 
